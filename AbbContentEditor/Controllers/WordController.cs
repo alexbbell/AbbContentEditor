877a735ab6090aa148d3093bc054be3f0fbbe20c
Aleksei Beliaev <Alexey.Beliaev@rocketta.de>
2026-08-02 23:44:18 +0200

    words CRUD methods

--- AbbContentEditor/Controllers/WordController.cs
+++ AbbContentEditor/Controllers/WordController.cs
@@ -1,9 +1,12 @@
 ﻿using AbbContentEditor.Data.Repositories;
 using AbbContentEditor.Data.UoW;
+using AbbContentEditor.Models;
 using AbbContentEditor.Models.Words;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 
@@ -17,40 +20,128 @@ namespace AbbContentEditor.Controllers
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly ILogger<WordController> _logger;
-        
+        private readonly UserManager<AbbAppUser> _userManager;
 
         public WordController(IRepository<WordCollection> wordColelctionRepository, IUnitOfWork unitOfWork, IMapper mapper,
-            ILogger<WordController> logger)
+            ILogger<WordController> logger, UserManager<AbbAppUser> userManager)
         {
             _wordColelctionRepository = wordColelctionRepository;
             _unitOfWork = unitOfWork;
             _mapper = mapper;
             _logger = logger;
+            _userManager = userManager;
         }
 
         [HttpGet]
-        [Route("wordcollections")]
+        [Route("collections")]
         [Produces("application/json")]
         [Authorize]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
 
-            string t = "b50bb495-4928-483a-b3b4-11afbe5e7b34";
-            //var r = _unitOfWork.wordCollectionRepository.Find(x => x.Where(x => x.Author.Id == t));;            
-            var r = _unitOfWork.wordCollectionRepository.GetAll().FirstOrDefault();// Find(x => x.Where(x => x.Author.Id == t)); ;
-            string strResult = JsonSerializer.Serialize(r);
-            string decodedText = Regex.Unescape(strResult);
-
-            return Ok(decodedText);
+            var user = await _userManager.FindByNameAsync(User.GetUsername()!);
+            if (user == null) return BadRequest();
+            IQueryable<WordCollectionBase> collections = _unitOfWork.wordCollectionRepository
+                    .GetAll().Where(x=>x.Author.Id ==  user.Id)
+                    .Select(x => x as WordCollectionBase);
+            return Ok(collections);
         }
 
         [HttpGet]
         [Route("collections/{id}")]
-        public IActionResult GetCollection(int id)
+        public async Task<IActionResult> GetCollection(int id)
         {
-            string t = "511fa4d8-cd51-48f9-a246-4ceaa84de246";
-            var r = _unitOfWork.wordCollectionRepository.Find(x => x.Where(x => x.Id == id));
+            
+            var user = await _userManager.FindByNameAsync(User.GetUsername()!);
+            if (user == null) return BadRequest();
+
+            var r = _unitOfWork.wordCollectionRepository.Find(x => x.Where(x => x.Id == id && x.Author.Id == user.Id)); 
             return Ok(JsonSerializer.Serialize(r));
+        }
+
+        [HttpPost]
+        [Route("collections")]
+        [Authorize]
+        public async Task<IActionResult> CreateCollection([FromBody] CreateWordCollectionDto collection)
+        {
+            if (collection == null)
+            {
+                return BadRequest("Collection cannot be null.");
+            }
+
+            var user = await _userManager.FindByNameAsync(User.GetUsername()!);
+            if (user == null) return BadRequest();
+            // Configure JSON serializer to use camelCase
+            var jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            var wordsList = collection.Words.Select(w => new Word
+            {
+                Translate1 = w.Translate1,
+                Translate2 = w.Translate2,
+                Translate3 = w.Translate3,
+            }).ToList();
+            // Transform DTO to Domain Entity
+            var collectionEntity = new WordCollection
+            {
+                Author = user,
+                Title = collection.Title,
+                WordsCollection = JsonDocument.Parse(JsonSerializer.Serialize(wordsList, jsonOptions))
+            };
+
+
+            await _unitOfWork.wordCollectionRepository.AddAsync(collectionEntity);
+            await _unitOfWork.Commit();
+            return CreatedAtAction(nameof(GetCollection), new { id = collectionEntity.Id }, collectionEntity);
+        }
+
+
+        [HttpPut]
+        [Route("collections/{id}")]
+        [Authorize]
+        public async Task<IActionResult> UpdateCollection(int id, [FromBody] CreateWordCollectionDto collection)
+        {
+            if (collection == null)
+            {
+                return BadRequest("Collection cannot be null.");
+            }
+            var user = await _userManager.FindByNameAsync(User.GetUsername()!);
+            if (user == null) return BadRequest();
+            var existingCollection = _unitOfWork.wordCollectionRepository.Find(x => x.Where(x => x.Id == id && x.Author.Id == user.Id)).FirstOrDefault();
+            if (existingCollection == null) return NotFound();
+            // Configure JSON serializer to use camelCase
+            var jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            var wordsList = collection.Words.Select(w => new Word
+            {
+                Translate1 = w.Translate1,
+                Translate2 = w.Translate2,
+                Translate3 = w.Translate3,
+            }).ToList();
+            existingCollection.Title = collection.Title;
+            existingCollection.WordsCollection = JsonDocument.Parse(JsonSerializer.Serialize(wordsList, jsonOptions));
+            await _unitOfWork.wordCollectionRepository.UpdateAsync(existingCollection);
+            await _unitOfWork.Commit();
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("collections/{id}")]
+        [Authorize]
+        public async Task<IActionResult> DeleteCollection(int id)
+        {
+            var user = await _userManager.FindByNameAsync(User.GetUsername()!);
+            if (user == null) return BadRequest();
+
+            var collection = _unitOfWork.wordCollectionRepository.Find(x => x.Where(x => x.Id == id && x.Author.Id == user.Id)).FirstOrDefault();
+            if (collection == null) return NotFound();
+
+            await _unitOfWork.wordCollectionRepository.DeleteAsync(collection);
+            await _unitOfWork.Commit();
+            return NoContent();
         }
     }
 }
--- AbbContentEditor/Models/Words/Word.cs
+++ AbbContentEditor/Models/Words/Word.cs
@@ -9,21 +9,32 @@ namespace AbbContentEditor.Models.Words
         public int Id {  get; set; }    
         public string Translate1 { get; set; }
         public string Translate2 { get; set; }
-        public string Translate3 { get; set; }
+        public string? Translate3 { get; set; }
     }
 
-    public class WordCollection : BaseClass 
+    
+    public class WordCollectionBase : BaseClass
     {
+        private string? WordsCollectionString { get; set; }
+        public AbbAppUser Author { get; set; }
+    }
 
+    public class WordCollection : WordCollectionBase
+    {
         public JsonDocument? WordsCollection
         {
             get => WordsCollectionString is null ? null : JsonDocument.Parse(WordsCollectionString);
             set => WordsCollectionString = value?.RootElement.GetRawText();
         }
-
         private string? WordsCollectionString { get; set; }
         // public JsonDocument WordsCollection { get; set; }
         public AbbAppUser Author { get; set; }
+    }
+
+    public class CreateWordCollectionDto
+    {
+        public string Title { get; set; }
+        public List<Word> Words { get; set; }
     }
 
 }
